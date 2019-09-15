6cd380686edc86ec235d0f5c60e19c55924cb38e
Daniel-Stafford <stafford.daniel@gmail.com>
2019-09-15 14:32:34 +0300

    refactored 8.5: Books by genre

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const uuid = require('uuid/v1')
 
 let authors = [
   {
@@ -96,7 +97,7 @@ const typeDefs = gql`
   type Author {
     name: String!
     id: ID!
-    born: Int!
+    born: Int
     bookCount: Int!
   }
 
@@ -104,8 +105,17 @@ const typeDefs = gql`
     hello: String!
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String! = "", genre: String! = ""): [Book!]
+    allBooks(author: String, genre: String): [Book!]
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
   }
 `
 
@@ -116,18 +126,16 @@ const resolvers = {
     authorCount: () => authors.length,
     allBooks: (root, args) => {
       //no parameters
-      if (args.author === '' && args.genre === '') return books
+      if (!args.author && !args.genre) return books
       //both parameters given
-      if (args.author.length > 0 && args.genre.length > 0)
+      if (args.author && args.genre)
         return books
           .filter(b => b.author === args.author)
           .filter(b => b.genres.includes(args.genre))
       //just author given
-      if (args.author.length > 0)
-        return books.filter(b => b.author === args.author)
+      if (args.author) return books.filter(b => b.author === args.author)
       //just genre given
-      if (args.genre.length > 0)
-        return books.filter(b => b.genres.includes(args.genre))
+      if (args.genre) return books.filter(b => b.genres.includes(args.genre))
     },
     allAuthors: () => authors
   },
