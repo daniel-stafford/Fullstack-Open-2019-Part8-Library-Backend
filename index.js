e73c0d08bd29c0463c5af923b2bc9b3272de27e4
Daniel-Stafford <stafford.daniel@gmail.com>
2019-09-21 11:08:08 +0300

    getting context not defined error

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ const server = new ApolloServer({
     const auth = req ? req.headers.authorization : null
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
       const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
-      const currentUser = await User.findById(decodedToken.id).populate('books')
+      const currentUser = await User.findById(decodedToken.id)
       return { currentUser }
     }
   }
